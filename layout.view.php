adc47dab30774a7d6ba3e675d778b0639cb67f0d
Jen Gray <69304519+jgray13@users.noreply.github.com>
2021-02-02 13:29:10 +0000

    Add files via upload

--- layout.view.php
+++ layout.view.php
@@ -8,14 +8,7 @@
  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
  -->
   </head>
-  <body>
-  
- 
-
   <?php
-  require("index.view.php");
+  //require("index.view.php");
   ?>
-  
-  
-  </body>
   </html>
--- php-array-practise.php
+++ php-array-practise.php
@@ -1,49 +1,99 @@
+<body>
+<main>
+<?php
+require("layout.view.php");
+?>
+<h1>PHP: Sorting Arrays</h1>
 <div class="outer">
 <div class="container-top">
 <?php
-require("layout.view.php");
-$animals = array('zebra', 'elephant', 'tiger', 'lion', 'whale', 'eel', 'duck', 'duck-billed-platypuss', 'monkey', 'dog', 'cat', 'mouse', 'parrot', 'shark', 'snake', 'wolf', 'aligator', 'crocodile', 'lionfish', 'panther', 'bug', 'spider', 'seahorse', 'horse', 'cow', 'pig', 'starfish', 'goose', 'hyena', 'owl', 'turkey', 'dolphin', 'donkey');
+$animals = array('zebra', 'elephant', 'tiger', 'lion', 'whale', 'duck', 'monkey', 'dog', 'cat', 'mouse', 'parrot', 'shark', 'snake', 'wolf', 'aligator', 'crocodile', 'lionfish', 'panther', 'spider', 'seahorse', 'horse', 'cow', 'pig', 'starfish', 'goose', 'hyena', 'owl', 'turkey', 'dolphin', 'donkey');
 $num_animals = count($animals);
-echo "<b><P>There are $num_animals animals in this array:</p></b><br>";
+echo "<b><P>There are $num_animals animals in this array:</p></b>";
+echo '<ul>';
 foreach ($animals as $value) {
-	echo $value . ', ';
+	echo '<li>' . $value . '</li>';
 }
+echo '</ul>';
 ?>
 </div>
-<div class="container">
+<div class="container-top">
 <?php
-$animal_sounds = array('zebra' => 'bray', 'elephant' => 'trumpet', 'tiger' => 'roar', 'lion' => 'roar', 'whale' => 'sing', 'eel' => 'slither', 'duck' => 'quack', 'duck-billed-platypuss' => '?', 'monkey' => 'screech', 'dog' => 'bark', 'cat' => 'meow', 'mouse' => 'squeek', 'parrot' => 'squawk', 'shark' => 'silent', 'snake' => 'hiss', 'wolf' => 'howl', 'aligator' => 'snap', 'crocodile' => 'snap', 'lionfish' => 'bubble', 'panther' => 'roar', 'bug' => 'crawl', 'spider' => 'creep', 'seahorse' => 'bubble', 'horse' => 'neigh', 'cow' => 'moo', 'pig' => 'oink', 'starfish' => 'bubble', 'goose' => 'honk', 'hyena' => 'laugh', 'owl' => 'hoot', 'turkey' => 'gobble', 'dolphin' => 'click', 'donkey' => 'bray');
-echo '<br><br><b><p>Animals and their sounds:</p></b>';
+$animal_sounds = array('zebra' => 'bray', 'elephant' => 'trumpet', 'tiger' => 'roar', 'lion' => 'roar', 'whale' => 'sing', 'duck' => 'quack', 'monkey' => 'screech', 'dog' => 'bark', 'cat' => 'meow', 'mouse' => 'squeek', 'parrot' => 'squawk', 'shark' => 'silent', 'snake' => 'hiss', 'wolf' => 'howl', 'aligator' => 'snap', 'crocodile' => 'snap', 'lionfish' => 'bubble', 'panther' => 'roar', 'spider' => 'creep', 'seahorse' => 'bubble', 'horse' => 'neigh', 'cow' => 'moo', 'pig' => 'oink', 'starfish' => 'bubble', 'goose' => 'honk', 'hyena' => 'laugh', 'owl' => 'hoot', 'turkey' => 'gobble', 'dolphin' => 'click', 'donkey' => 'bray');
+echo '<b><p>Animals and their sounds:</p></b>';
+echo '<ul>';
 foreach ($animal_sounds as $key => $value) {
-	echo '<br>&bull;', $key . ' - ' . $value;
+	echo '<li>', $key . ' - ' . $value . '</li>';
 }
-?>
-</div>
-<div class="container">
-<?php
-asort($animal_sounds);
-echo '<br><br><b><p>Sorted into value order - asort():</p></b>';
-foreach ($animal_sounds as $key => $value) {
-    echo '<br>&bull;', $key . ' - ' . $value;
-}
-?>
-</div>
-<div class="container">
-<?php
-ksort($animal_sounds);
-echo '<br><br><b><p>Sorted into key order - ksort():</p></b>';
-foreach ($animal_sounds as $key => $value) {
-	echo '<br>&bull;', $key . ' - ' . $value;
-}
+echo '</ul>';
 ?>
 </div>
 <div class="container">
 <?php
 sort($animal_sounds);
-echo '<br><br><b><p>Sorted into value order - without keys - sort():</p></b>';
+echo '<b><p>Sorted into ascending value order - without keys - sort():</p></b>';
+echo '<ul>';
 foreach ($animal_sounds as $key => $value) {
-	echo '<br>&bull;', $key . ' - ' . $value;
+	echo '<li>', $key . ' - ' . $value . '</li>';
 }
+echo '</ul>';
+?>
+</div>
+<div class="container">
+<?php
+asort($animal_sounds);
+echo '<b><p>Sorted into ascending value order - asort():</p></b>';
+echo '<ul>';
+foreach ($animal_sounds as $key => $value) {
+    echo '<li>', $key . ' - ' . $value . '</li>';;
+}
+echo '</ul>';
+?>
+</div>
+<div class="container">
+<?php
+ksort($animal_sounds);
+echo '<b><p>Sorted into ascending key order - ksort():</p></b>';
+echo '<ul>';
+foreach ($animal_sounds as $key => $value) {
+	echo '<li>', $key . ' - ' . $value . '</li>';;
+}
+echo '</ul>';
+?>
+</div>
+<div class="container">
+<?php
+rsort($animal_sounds);
+echo '<b><p>Sorted into descending value order - without keys - rsort():</p></b>';
+echo '<ul>';
+foreach ($animal_sounds as $key => $value) {
+	echo '<li>', $key . ' - ' . $value . '</li>';
+}
+echo '</ul>';
+?>
+</div>
+<div class="container">
+<?php
+arsort($animal_sounds);
+echo '<b><p>Sorted into descending value order - arsort():</p></b>';
+echo '<ul>';
+foreach ($animal_sounds as $key => $value) {
+    echo '<li>', $key . ' - ' . $value . '</li>';
+}
+echo '</ul>';
+?>
+</div>
+<div class="container">
+<?php
+krsort($animal_sounds);
+echo '<b><p>Sorted into descending key order - krsort():</p></b>';
+echo '<ul>';
+foreach ($animal_sounds as $key => $value) {
+	echo '<li>', $key . ' - ' . $value . '</li>';
+}
+echo '</ul>';
 ?>
 </div>
 </div> <!-- end outer -->
+</main>
+</body>
